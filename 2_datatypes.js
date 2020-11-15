133cb3ee3217212ead5aa608012fb42f090dc717
tariqulislam1011 <tariqulislamabc1011@gmail.com>
2020-11-15 14:52:12 +0600

    day 2 data types

---
+++ 2_datatypes.js
@@ -0,0 +1,17 @@
+
+
+function main() {
+    var myInt, myFloat, myString;
+
+    
+    myInt = parseInt(readLine());
+    myFloat = parseFloat(readLine());
+    myString = readLine();
+  
+   
+    console.log(i + myInt);
+  
+    console.log((d + myFloat).toFixed(1));
+  
+    console.log(s + myString)
+}
