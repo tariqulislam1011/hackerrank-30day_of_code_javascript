f384eb5a2d5ffdf996f1b322963e1eee47a210ca
tariqulislam1011 <tariqulislamabc1011@gmail.com>
2020-11-17 02:23:56 +0600

    day 5 loops

--- 4_class_vs_instance.js
+++ 4_class_vs_instance.js
@@ -3,10 +3,12 @@ function Person(initialAge){
   var age;
   if (initialAge >= 0) {
     age = initialAge
-  } else {
+  }
+   else {
     age = 0;
     console.log('Age is not valid, setting age to 0.');
   }
+
 
   this.amIOld=function(){
    // Do some computations in here and print out the correct statement to the console
