d5d62f1222a9d5bc877fee4e25f15981f037eea9
tariqulislam1011 <tariqulislamabc1011@gmail.com>
2020-11-23 22:32:00 +0600

    day 9 recursion

---
+++ 9_dayRecursion.js
@@ -0,0 +1,22 @@
+function processData(input) {
+
+    function factorial(n){
+        if(n==1 || n==0){
+            return 1;
+        }else{
+            return n * factorial(n-1);
+        }
+    }
+    console.log(factorial(input));
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
